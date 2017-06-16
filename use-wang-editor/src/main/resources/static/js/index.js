f52f97c9148f5c8807ec12ab7f3424d718a6e409
janita <zcj880902>
2017-06-16 18:03:54 +0800

    使用 wangEditor 富文本编辑器

--- use-wang-editor/src/main/resources/static/js/index.js
+++ use-wang-editor/src/main/resources/static/js/index.js
@@ -14,4 +14,20 @@ function load() {
  */
 $(function () {
     load();
-})
+
+});
+
+/**
+ * 创建富文本编辑器
+ * @param domId 创建编辑器的id
+ * @param actionUrl 上传图片请求路径
+ * @param imgName   上传图片的名字
+ * @returns {wangEditor}    编辑器实例
+ */
+function createEditor(domId, actionUrl, imgName) {
+  var editor = new wangEditor(domId);
+  editor.config.uploadImgUrl = actionUrl;
+  editor.config.uploadImgFileName = imgName;
+  editor.create();
+  return editor;
+};
