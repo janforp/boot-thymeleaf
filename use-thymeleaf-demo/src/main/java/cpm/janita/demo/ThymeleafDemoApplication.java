8f09948ebeeb32bc763287cebf8e4968b99b1389
janita <zcj880902>
2017-06-16 14:02:53 +0800

    使用thymeleaf

---
+++ use-thymeleaf-demo/src/main/java/cpm/janita/demo/ThymeleafDemoApplication.java
@@ -0,0 +1,16 @@
+package cpm.janita.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/16 0016-下午 1:43
+ * 该类是：
+ */
+@SpringBootApplication
+public class ThymeleafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafDemoApplication.class, args);
+    }
+}
