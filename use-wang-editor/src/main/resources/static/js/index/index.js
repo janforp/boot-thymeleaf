fc24265cbab329789c2e522d52f1dacee0d885e2
janforp <janforp@163.com>
2017-06-18 10:19:13 +0800

    测试

---
+++ use-wang-editor/src/main/resources/static/js/index/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Janita on 2017/6/16
+ */
+
+/**
+ * 页面加载完成后
+ */
+$(function () {
+    utils.print("页面加载完毕!");
+});
---
+++ use-wang-editor/src/main/resources/static/js/utils/CommonUtils.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Janita on 2017/6/17.
+ */
+var utils = {
+    /**
+     * 打印
+     * @param msg
+     */
+    print:function (msg) {
+        console.log(msg);
+    }
+};
